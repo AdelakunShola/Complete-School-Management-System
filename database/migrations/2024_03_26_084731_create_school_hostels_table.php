5f363e5bf93c798fb4003f4ffe99b3041fa91191
Adelakun Shola <officialadelakun@gmail.com>
2024-03-25 21:53:58 -1100

    school hostel db bug fixed

--- database/migrations/2024_03_26_084731_create_school_hostels_table.php
+++ database/migrations/2024_03_26_084731_create_school_hostels_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('description')->nullable();
             $table->timestamps();
         });
-    } 
+    }
 
     /**
      * Reverse the migrations.
